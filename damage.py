e9fc3956732cf2a8c0775235562f6cb2a4d3abf7
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2025-12-24 16:53:56 -0800

    adding cleave attack

--- damage.py
+++ damage.py
@@ -31,19 +31,32 @@ def large_damage_great_weapon():  # large damage with 1 and 2 increased to 3
         die = 3
     return die
 
+def cleave():
+    return roll_d12()+1
+
+def cleave_great_weapon():
+    die = roll_d12()
+    if die in [1,2]:
+        die = 3
+    return die+1
+
 def savage_round():
     first_attack = savage_attacker()
     second_attack = regular_attack()
     first_large = large_dmg()
     second_large = large_dmg()
-    return (first_attack+second_attack+first_large+second_large)
+    cleave_attack = cleave()
+    cleave_large = large_dmg()
+    return (first_attack+second_attack+first_large+second_large+cleave_attack+cleave_large)
 
 def great_weapon_round():
     first_attack = regular_attack_great_weapon()
     second_attack = regular_attack_great_weapon()
     first_large = large_damage_great_weapon()
     second_large = large_damage_great_weapon()
-    return (first_attack+second_attack+first_large+second_large)
+    cleave_attack = cleave_great_weapon()
+    cleave_large = large_dmg()
+    return (first_attack+second_attack+first_large+second_large+cleave_attack+cleave_large)
 
 if __name__ == '__main__':
     savage_attacker_turns = []
